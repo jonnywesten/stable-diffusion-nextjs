fb45bbf1b8cf19ed0e8fdd42890ac46fc2b0b09e
DanielElport <dan.elport@gmail.com>
2023-12-22 22:47:40 +0100

    format

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,8 @@
 'use server'
 import axios, {RawAxiosRequestHeaders} from "axios";
 
-/*
-styles:
-3d-model analog-film anime cinematic comic-book digital-art enhance fantasy-art isometric line-art low-poly modeling-compound neon-punk origami photographic pixel-art tile-texture
- */
 export async function callGenerate(prompt: string) {
+
     const token = process.env.WIZMODEL_TOKEN
     const url = "https://api.wizmodel.com/v1/predictions"
     const payload = {
