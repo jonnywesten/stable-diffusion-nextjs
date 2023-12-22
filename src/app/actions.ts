6fedc20f309eade8aefbc232a37f5da295617bcd
DanielElport <dan.elport@gmail.com>
2023-12-22 22:20:16 +0100

    format

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -10,19 +10,18 @@ export async function callGenerate(prompt: string) {
     const url = "https://api.wizmodel.com/v1/predictions"
     const payload = {
         "version": "7d229e3ed5d01c879622d0cd273572260b7e35103d6765af740f853b160d04b7",
-    input:{
-        prompt,
-        steps: 25,
-        width: 512,
-        height: 512,
-    }
+        input: {
+            prompt,
+            steps: 25,
+            width: 512,
+            height: 512,
+        }
     }
 
     const headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + token
     } as RawAxiosRequestHeaders
-
 
     const start = new Date()
     const response = await axios.post(url, payload, {headers})
