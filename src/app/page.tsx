fe0c1bbaa15fc59120a02228a9d1b799d97dcaf6
DanielElport <dan.elport@gmail.com>
2023-12-23 11:52:38 +0100

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             <LoadingSpinnerComponent loading={loading}/>
             <ThemeToggle/>
             <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
-                <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl pr-8"><span
+                <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl pr-12 sm:pr-0"><span
                     className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">NextJS </span>Stable
                     Diffusion</h1>
                 <p className="text-lg mt-2 text-center font-normal text-gray-500 lg:text-xl dark:text-gray-400 mb-12 max-w-[700px]">Turn
